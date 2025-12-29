f5266571d0d2e6a271acfed66de3c15ebb536d08
Alaa1998110 <elsharkawya575@gmail.com>
2025-12-29 19:27:11 +0200

    added wait for database option

--- app/app/tests.py
+++
@@ -1,8 +0,0 @@
-from . import calc
-from django.test import SimpleTestCase
-
-class CalcTests(SimpleTestCase):
-
-    def test_add_numbers(self):
-        result = calc.add(1,2)
-        self.assertEqual(result,3)
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,27 @@
+
+"""
+Django command to wait for the database to be available.
+"""
+from django.core.management.base import BaseCommand
+import time
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as psycopg20pError
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+        db_up=False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up=True
+
+            except (OperationalError,psycopg20pError):
+                self.stdout.write('Database unavailable , wait 1 second..')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database available'))
+
+
